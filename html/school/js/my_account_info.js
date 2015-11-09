bd74157b00986500fbfbca702347858b85096ddf
pennwin2014 <pennwin2014@gmail.com>
2015-11-09 10:35:43 +0800

    完成bug的修改20151109

---
+++ html/school/js/my_account_info.js
@@ -0,0 +1,33 @@
+﻿function a()
+{
+	jumpToPage('pgCanChangePackage');
+}
+
+function b()
+{
+	alert ("立即充值");
+}
+function c()
+{
+	alert ("充值记录");
+}
+
+function d()
+{
+	alert ("账单查询");
+}
+$(document).on("pagebeforeshow","#pgMyInfo",function()
+{
+	document.getElementById("current_package").innerHTML ="500M无忧套餐";
+	document.getElementById("current_package").style.color ="#AFAFAF";
+	document.getElementById("valid_date").innerHTML ="2015-03-01至2015-3-31";
+	document.getElementById("valid_date").style.color ="#AFAFAF";
+	document.getElementById("account_balance").innerHTML ="20元";
+	document.getElementById("account_balance").style.color ="#AFAFAF";
+	document.getElementById("data_package").innerHTML ="流量套餐";
+	document.getElementById("data_package").style.color ="#AFAFAF";
+	document.getElementById("package_content").innerHTML="每月享有500M上网流量";
+	document.getElementById("package_content").style.color ="#AFAFAF";
+	document.getElementById("progress").innerHTML="50%";
+	document.getElementById("bar").style.width="50%";
+});
---
+++ html/school/js/recharge_record.js
@@ -0,0 +1,81 @@
+﻿//账单数据存储结构
+	var recharge_record_arr = [
+		{
+			'date':'2015-11-04    14:25',
+			'menoy':'100元',
+			'pay_mode':'微信支付'
+		},{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+		,{
+			'date':'2015-10-12    12:25',
+			'menoy':'20元',
+			'pay_mode':'支付宝'
+		}
+	]
+
+	
+	$(document).on("pagebeforeshow","#pgRechargeRecord",function(){
+		//加载数据
+		//record_div增量
+		var increment = 0;
+		//record_div id 变量
+		var record_div_id ="";
+		//遍历账单信息数组
+		$.each(recharge_record_arr,function(n,recharge_record_arr) {
+			//创建外层div
+			record_div_id="record_div_id"+increment;
+			var $record_div=$("<div id='"+ record_div_id +"'></div>");
+			$("#recharge_record").append($record_div);
+			$("#"+record_div_id).addClass("record_div_style");
+			//将账单信息加载到record_div
+			var $mydate=$("<b>"+ recharge_record_arr.date +"</b><br>");
+			var $money=$("<b>金额:</b><span>"+ recharge_record_arr.menoy +"</span><br>");
+			var $pay_mode=$("<b>付款方式:</b><span>"+ recharge_record_arr.pay_mode +"</span><br>");
+			$("#"+record_div_id).append($mydate,$money,$pay_mode);
+			$("span").addClass("myStyle");
+			increment ++;
+        });  
+	});
